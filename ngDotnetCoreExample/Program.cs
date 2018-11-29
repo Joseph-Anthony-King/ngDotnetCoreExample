8ef34bb24b10a2cde611ff2f2860fd5632380dd0
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2018-11-28 21:30:52 -0800

    Integrate the ClientApp

--- ngDotnetCoreExample/Program.cs
+++ ngDotnetCoreExample/Program.cs
@@ -8,12 +8,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ngDotnetCoreExample
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace ngDotnetCoreExample {
+
+    public class Program {
+        public static void Main(string[] args) {
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
