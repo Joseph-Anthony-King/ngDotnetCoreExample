1e69aeb4cccc895f262eb2d9c31ebbfaff5e10e1
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2018-12-03 09:41:10 -0800

    Move routes to const variables

--- ngDotnetCoreExample/ClientApp/src/app/home/home.module.ts
+++ ngDotnetCoreExample/ClientApp/src/app/home/home.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 
 import { HomeComponent } from './home.component';
 
+const routes: Routes = [
+    { path: "home", component: HomeComponent },
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "**", redirectTo: "home", pathMatch: "full" }
+]
+
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([
-        { path: "home", component: HomeComponent },
-        { path: "", redirectTo: "home", pathMatch: "full" },
-        { path: "**", redirectTo: "home", pathMatch: "full" }
-    ])
-  ]
+    declarations: [HomeComponent],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes)
+    ]
 })
 export class HomeModule { }
--- ngDotnetCoreExample/ClientApp/src/app/values/values.module.ts
+++ ngDotnetCoreExample/ClientApp/src/app/values/values.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 
 import { ValuesComponent } from './values.component';
 
+const routes: Routes = [
+    { path: "values", component: ValuesComponent }
+]
+
 @NgModule({
-  declarations: [ValuesComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([
-        { path: "values", component: ValuesComponent }
-    ])
-  ]
+    declarations: [ValuesComponent],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes)
+    ]
 })
 export class ValuesModule { }
