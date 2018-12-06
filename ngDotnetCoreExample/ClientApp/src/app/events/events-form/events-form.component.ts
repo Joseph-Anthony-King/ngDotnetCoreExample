d2ecba8dc82e5682d7f0d536e04afc5d31ad8050
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2018-12-05 17:06:57 -0800

    Add events form

---
+++ ngDotnetCoreExample/ClientApp/src/app/events/events-form/events-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-events-form',
+    templateUrl: './events-form.component.html',
+    styleUrls: ['./events-form.component.css']
+})
+export class EventsFormComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
--- ngDotnetCoreExample/ClientApp/src/app/events/events.module.ts
+++ ngDotnetCoreExample/ClientApp/src/app/events/events.module.ts
@@ -7,14 +7,16 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { EventsCalendarComponent } from './events-calendar/events-calendar.component';
 import { UtilsModule } from '../utils/utils.module';
 import { EventsSchedulerComponent } from './events-scheduler/events-scheduler.component';
+import { EventsFormComponent } from './events-form/events-form.component';
 
 const routes: Routes = [
     { path: 'calendar', component: EventsCalendarComponent },
-    { path: 'schedule', component: EventsSchedulerComponent }
+    { path: 'schedule', component: EventsSchedulerComponent },
+    { path: 'edit-form', component: EventsFormComponent }
 ]
 
 @NgModule({
-    declarations: [EventsCalendarComponent, EventsSchedulerComponent],
+    declarations: [EventsCalendarComponent, EventsSchedulerComponent, EventsFormComponent],
     imports: [
         CommonModule,
         UtilsModule,
